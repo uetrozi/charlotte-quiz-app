7ea63003bf0ca7e3b8f8764fd21ed6f10b3416d9
uetrozi <charlotte_keuer@posteo.de>
2023-11-13 12:37:47 +0100

    add functionality to bookmark and show answer button

---
+++ index.js
@@ -0,0 +1,29 @@
+console.clear();
+
+const bookmark = document.querySelector('img[data-js="question-card__icon"]');
+
+bookmark.addEventListener("click", () => {
+  bookmark.classList.toggle("filled");
+
+  if (bookmark.classList.contains("filled")) {
+    bookmark.setAttribute("src", "./Images/bookmark_filled.png");
+  } else {
+    bookmark.setAttribute("src", "./Images/bookmark.png");
+  }
+});
+
+const showAnswerButton = document.querySelector(
+  '[data-js="question-card__button"]'
+);
+const answer = document.querySelector('[data-js="question-card__answer"]');
+
+function toggleButtonText() {
+  showAnswerButton.textContent == "Show Answer"
+    ? (showAnswerButton.textContent = "Hide Answer")
+    : (showAnswerButton.textContent = "Show Answer");
+}
+
+showAnswerButton.addEventListener("click", () => {
+  answer.classList.toggle("hidden");
+  toggleButtonText();
+});
